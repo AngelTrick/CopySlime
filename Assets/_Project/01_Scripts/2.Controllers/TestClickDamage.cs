1742411de90979015a70cdf178f86ba9fa1ffdc4
zingkyh135 <zingkyh@gmail.com>
2026-03-17 15:38:33 +0900

    [Add] 골드 자석 구현

--- Assets/_Project/01_Scripts/2.Controllers/TestClickDamage.cs
+++ Assets/_Project/01_Scripts/2.Controllers/TestClickDamage.cs
@@ -10,15 +10,36 @@ public class TestClickDamage : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Monster[] monsters = FindObjectsOfType<Monster>();
+            TreasureChest[] chests = FindObjectsOfType<TreasureChest>();
 
             var targetMonster = monsters
                 .Where(m => m.gameObject.activeSelf)
                 .OrderBy(m => m.transform.position.x)
                 .FirstOrDefault();
 
-            if (targetMonster != null)
+            var targetChest = chests
+                .Where(c => c.gameObject.activeSelf)
+                .OrderBy(c => c.transform.position.x)
+                .FirstOrDefault();
+
+            if (targetMonster != null && targetChest != null)
+            {
+                if (targetMonster.transform.position.x < targetChest.transform.position.x)
+                {
+                    targetMonster.TakeDamage(10f);
+                }
+                else
+                {
+                    targetChest.TakeDamage(10f);
+                }
+            }
+            else if (targetMonster != null)
             {
                 targetMonster.TakeDamage(10f);
+            }
+            else if (targetChest != null)
+            {
+                targetChest.TakeDamage(10f);
             }
         }
     }
