506d289c9ee8089341711cae8954dcc40e786aef
dbxbqmdyd <dbxbqmdyd019@gmail.com>
2026-03-23 22:21:51 +0900

    UI 스킬버튼 수정

--- Assets/_Project/01_Scripts/4.UI/UISkill.cs
+++ Assets/_Project/01_Scripts/4.UI/UISkill.cs
@@ -1,23 +1,120 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.UI;
 
 public class UISkill : MonoBehaviour
 {
-    [Header("잠금 설정")]
-    public int unlockLevel; // 몇 레벨에 해금되는지 or 보스 클리어나 스테이지로 변경 가능
+    [Header("스킬 버튼")]
     public Button skillButton;
-    public Image lockImage; // 잠금 이미지 / 이미지 없으면 그냥 비활성화로 처리예정
+    [Header("쿨타임")]
+    public Image cooldownImage;
+    [Header("공유")]
+    public UISkill pairingSkill;
+    [Header("잠금")]
+    public Image lockImage;
+    public bool isSkillBI = false; // SkillButton_Image 면 true 체크
 
+    public int unlockLevel = 1;  // 해금 레벨
+
+    private bool isCooldown = false;
+    private bool isLocked = true;
+
+    private float duration = 3f;
+    private float elapsed = 0f;
     void Start()
     {
-        SetLock(true); 
-    }
+        skillButton.onClick.AddListener(OnClick);
+        cooldownImage.fillAmount = 0f;
 
-    public void SetLock(bool isLocked)
+        if (isSkillBI && pairingSkill != null) unlockLevel = pairingSkill.unlockLevel;
+
+        SetLock(true);
+        if (!isSkillBI)
+        {
+            UISkillUpdate usu = FindObjectOfType<UISkillUpdate>();
+            if (usu != null) usu.RegisterSkill(this);
+        }
+    }
+    public void SetLock(bool locked)
     {
-        skillButton.interactable = !isLocked;
-        lockImage.gameObject.SetActive(isLocked); 
+        isLocked = locked;
+        skillButton.interactable = !locked;
+
+        if (lockImage != null) lockImage.gameObject.SetActive(locked);
+        if (pairingSkill != null) pairingSkill.SetPreventLock(locked);
+
+    }
+    public void SetPreventLock(bool locked) //루프방지
+    {
+        isLocked = locked;
+        skillButton.interactable = !locked;
+
+        if (lockImage != null) lockImage.gameObject.SetActive(locked);
+
+    }
+    void OnClick()
+    {
+        if (isCooldown || isLocked) return;
+
+        TempSkill(); // SO 대체하기
+
+        Debug.Log($"{gameObject.name} 클릭!");
+        StartCooldown();
+
+        if (pairingSkill != null) pairingSkill.StartCooldownPair();
+    }
+    void TempSkill()
+    {
+        Debug.Log($"스킬 실행"); // 나중에 SO 있으면 그걸로
+    }
+    void StartCooldown()
+    {
+        isCooldown = true;
+        elapsed = 0f;
+        skillButton.interactable = false;
+        cooldownImage.fillAmount = 1f;
+    }
+    public void StartCooldownPair()
+    {
+        if (!isCooldown)
+        {
+            isCooldown = true;
+            elapsed = 0f;
+            skillButton.interactable = false;
+            cooldownImage.fillAmount = 1f;
+        }
+    }
+    // 코루틴으로 하니깐 창 비활성화시 오류가 발생함 좋은 방법이 생각나기전까 업데이트로 구현
+    public void ManualUpdate()
+    {
+        if (!isCooldown) return;
+
+        elapsed += Time.deltaTime;
+        float fill = 1f - (elapsed / duration);
+
+        cooldownImage.fillAmount = fill;
+        if (pairingSkill != null) pairingSkill.cooldownImage.fillAmount = fill;
+
+
+        if (elapsed >= duration) // 쿨탐 종료
+        {
+            isCooldown = false;
+            elapsed = 0f;
+            cooldownImage.fillAmount = 0f;
+
+            if (!isLocked) skillButton.interactable = true;
+
+            if (pairingSkill != null)
+            {
+                pairingSkill.cooldownImage.fillAmount = 0f;
+                pairingSkill.isCooldown = false;
+                pairingSkill.elapsed = 0f;
+
+                if (!pairingSkill.isLocked) pairingSkill.skillButton.interactable = true;
+
+            }
+        }
     }
 }
