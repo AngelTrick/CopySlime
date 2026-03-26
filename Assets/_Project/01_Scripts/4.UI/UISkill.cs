c4bb3f9ba81fade76b5ad3c340d208df6c23d35b
dbxbqmdyd <dbxbqmdyd019@gmail.com>
2026-03-26 11:11:44 +0900

    변수 변경

--- Assets/_Project/01_Scripts/4.UI/UISkill.cs
+++ Assets/_Project/01_Scripts/4.UI/UISkill.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.UI;
 
 public class UISkill : MonoBehaviour
@@ -10,6 +8,8 @@ public class UISkill : MonoBehaviour
     public Button skillButton;
     [Header("쿨타임")]
     public Image cooldownImage;
+    public TextMeshProUGUI cooldownText;
+    public Color cooldownTextColor = Color.white; // 쿨타임 텍스트 색상
     [Header("공유")]
     public UISkill pairingSkill;
     [Header("잠금")]
@@ -28,6 +28,11 @@ public class UISkill : MonoBehaviour
         skillButton.onClick.AddListener(OnClick);
         cooldownImage.fillAmount = 0f;
 
+        if (cooldownText != null)
+        {
+            cooldownText.gameObject.SetActive(false);
+            cooldownText.color = cooldownTextColor;
+        }
         if (isSkillBI && pairingSkill != null) unlockLevel = pairingSkill.unlockLevel;
 
         SetLock(true);
@@ -74,7 +79,11 @@ public class UISkill : MonoBehaviour
         isCooldown = true;
         elapsed = 0f;
         skillButton.interactable = false;
-        cooldownImage.fillAmount = 1f;
+        cooldownImage.fillAmount = 0f;
+        cooldownImage.color = new Color(cooldownImage.color.r,
+                                cooldownImage.color.g,
+                                cooldownImage.color.b, 0.4f);
+        if (cooldownText != null) cooldownText.gameObject.SetActive(true);
     }
     public void StartCooldownPair()
     {
@@ -83,26 +92,43 @@ public class UISkill : MonoBehaviour
             isCooldown = true;
             elapsed = 0f;
             skillButton.interactable = false;
-            cooldownImage.fillAmount = 1f;
+            cooldownImage.fillAmount = 0f;
+            cooldownImage.color = new Color(cooldownImage.color.r,
+                                cooldownImage.color.g,
+                                cooldownImage.color.b, 0.4f);
+            if (cooldownText != null) cooldownText.gameObject.SetActive(true);
         }
     }
     // 코루틴으로 하니깐 창 비활성화시 오류가 발생함 좋은 방법이 생각나기전까 업데이트로 구현
+    // 3. 알파값 0 -> 뒤에 블럭깔아놔서 터치안되게 막기 -> 기본이 완성되고 디테일올릴때 생각 해본걸로 개선하기
     public void ManualUpdate()
     {
         if (!isCooldown) return;
 
         elapsed += Time.deltaTime;
-        float fill = 1f - (elapsed / duration);
+        float fill = elapsed / duration;
+        float remaining = duration - elapsed;
 
         cooldownImage.fillAmount = fill;
-        if (pairingSkill != null) pairingSkill.cooldownImage.fillAmount = fill;
+        if (cooldownText != null) cooldownText.text = remaining.ToString("F1");
 
+        if (pairingSkill != null)
+        {
+            pairingSkill.cooldownImage.fillAmount = fill;
+
+            if (pairingSkill.cooldownText != null)
+            {
+                pairingSkill.cooldownText.text = remaining.ToString("F1");
+            }
+        }
 
         if (elapsed >= duration) // 쿨탐 종료
         {
             isCooldown = false;
             elapsed = 0f;
             cooldownImage.fillAmount = 0f;
+
+            if (cooldownText != null) cooldownText.gameObject.SetActive(false);
 
             if (!isLocked) skillButton.interactable = true;
 
@@ -112,6 +138,10 @@ public class UISkill : MonoBehaviour
                 pairingSkill.isCooldown = false;
                 pairingSkill.elapsed = 0f;
 
+                if (pairingSkill.cooldownText != null)
+                {
+                    pairingSkill.cooldownText.gameObject.SetActive(false);
+                }
                 if (!pairingSkill.isLocked) pairingSkill.skillButton.interactable = true;
 
             }
