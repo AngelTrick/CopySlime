e60edbbcdf94ba9bd834568b1912d537a9f9cd88
zingkyh135 <zingkyh@gmail.com>
2026-04-01 17:26:10 +0900

    [Add] 스테이지 UI, 체력바 추가
    스테이지 번호, 이름 출력 UI 생성
    
    몬스터 체력바 구현
    
    기존 플레이어 태그 인식 > 레이어 인식으로 수정

---
+++ Assets/_Project/01_Scripts/2.Controllers/StageNumber.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StageNumber : MonoBehaviour
+{
+    [Header("텍스트 연결")]
+    public TextMeshProUGUI stageText; //스테이지 번호
+    public TextMeshProUGUI stageName; //스테이지 이름
+
+    void Update()
+    {
+        if (StageManager.Instance == null) return;
+
+        int currentLevel = StageManager.Instance.GetCurrentLevel();
+        if (stageText != null)
+        {
+            stageText.text = "STAGE " + currentLevel;
+        }
+
+        string currentName = StageManager.Instance.GetStageName();
+        if (stageName != null)
+        {
+            stageName.text = currentName;
+        }
+    }
+}
